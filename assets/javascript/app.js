d0c2a18b9e412fa016c932b48268a3d0417b6e82
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-09-14 01:40:03 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //onload function displays a play button, which class question 1 from the game object
 window.onload = function(){
 	$('#display').html('<span id="play">play</span>')
-	$('#play').click(game.StartTimer);
+	$('#play').click(game.Q1);
 
 }
 
@@ -9,8 +9,14 @@ window.onload = function(){
 var clockRunning = false;
 //global variables
 var counter;
-var correct;
-var unanswered;
+var correct=0;
+var incorrect=0;
+var unanswered=0;
+var question;
+var a;
+var b;
+var c; 
+var d;
 
 //this is the game object, which controls the timer as well as the trivia questions.
 var game ={
@@ -18,28 +24,70 @@ var game ={
 		time:20,
 		
 
-	//this function calls the function calls the function decrement every second.
-	StartTimer: function(){ 
-		//  TODO: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
+	//this function calls the function decrement every second.
+	//it also asks the first question and links the question, asnwers and timer to the user Interface.
+	Q1: function(){
+		if (!clockRunning) {
         clockRunning=true;
         counter=setInterval(game.decrement,1000);
       }
 
+      question = "Who wrote the write of spring?"
+      a= "Stravinsky";
+      b= "Bach";
+      c= "Mozart";
+      d= "Brahms";
+      game.interface();
+
+      if($('#a').click())
+      	{
+      	console.log("this is working");
+	     }
 
 	},
 	//this function subtracts game.time by one every time it is called and displays the current game.time in the UI.
 	decrement: function(){
 		game.time--;
 
-		$('#display').html(game.time);		
+		$('#timer').html(game.time);		
 	},
 
 	stop: function(){
 		clearInterval(counter);
 		clockRunning=false;
-	}
+	},
 
+	//this function resets the game.
+	reset: function (){
+		$('#resetGame').click(game.Q1);
+
+	},
+
+	
+	//this function is the userInterface that displays when the user is answering questions.
+	interface: function(){
+		var countDown = "<h1>" + game.decrement + "</h1>" 
+
+		var html= 
+				  "<h1>" + question + "</h1>" +
+				  "<span class = 'a'>" + a + "</span>"+"<br/>"+
+				  "<span class = 'b'>" + b + "</span>"+"<br/>"+
+				  "<span class = 'c'>" + c + "</span>"+"<br/>"+
+				  "<span class = 'd'>" + d + "</span>"
+
+		$('#display').html(html);		  
+	},			 
+	//this function displays the user's results when the game is over and gives them the option to play again.
+	result: function(){ 
+
+		var end= 
+				  "<h1>" + "Correct:" + correct + "</h1>" +
+				  "<h1>" + "ncsorrect:" + incorrect + "</h1>" +
+				  "<h1>" + "unanswered:" + unanswered + "</h1>" +
+				  "<span id='resetGame'>" + "Play Again"  + "</span>"
+
+		$('#display').html(end);		  
+	}		 		
 	
 
 }
