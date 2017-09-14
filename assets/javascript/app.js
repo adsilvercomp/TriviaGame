e17ade19f33666b4bc2f95ce91b47c1b379572ed
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-09-14 15:59:36 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //onload function displays a play button, which calls question 1 from the game object
-window.onload = function(){
-	$('#display').html('<span id="play">play</span>')
-	$('#play').click(game.start);
+window.onload = function() {
+    $('#display').html('<span id="play">play</span>')
+    $('#play').click(game.start);
 
 }
 
@@ -9,123 +9,208 @@ window.onload = function(){
 var clockRunning = false;
 //global variables
 var counter;
-var correct=0;
-var incorrect=0;
-var unanswered=0;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 var question;
 var a;
 var b;
-var c; 
+var c;
 var d;
 
 
 //this is the game object, which controls the timer as well as the trivia questions.
-var game ={
+var game = {
 
-		time:20,
-		
+    time: 20,
+    aTime: 10,
 
-	//this function calls the function Q1 every second.
-	start: function(){
-		if (!clockRunning) {
-        clockRunning=true;
-        counter=setInterval(game.Q1, 1000);
-      }
-
-      
-
-	},
-	//this function subtracts game.time by one every time it is called and displays the current game.time in the UI.
-	//it also asks the first multiple choice question and links the question, asnwers and timer to the user Interface.
-	Q1: function(){
-		//this decrements game.time by 1 every second
-		game.time--;
+    //this function calls the function Q1 every second.
+    start: function() {
+        if (!clockRunning) {
+            clockRunning = true;
+            counter = setInterval(game.Q1, 1000);
+        }
 
 
-		//console.log should print only when clicked, but it is printing every time the function is called by start.
-		if($('.a').click())
-      	{
-      	console.log("this is working");
-	     }
 
-	    //this is printing out on the user interface. 
-		question = "Who wrote the Rite of Spring?"
-      a= "Stravinsky";
-      b= "Bach";
-      c= "Mozart";
-      d= "Brahms";
-      game.interface();
-
-      	
-      	//if the timer hits 0, the stop function is called, and answer 1 is called.
-	     if(game.time===0){
-	     	game.stop();
-	     	unanswered++;
-	     	game.answer1();
-	     }
-	},
-
-	stop: function(){
-		clearInterval(counter);
-		clockRunning=false;
-	},
-
-	//this function resets the game.
-	reset: function (){
-		$('#resetGame').click(game.Q1);
-
-	},
-
-	
-	//this function is the userInterface that displays when the user is answering questions.
-	interface: function(){
-		
-
-		var html= 
-				  "<h1 class = 'question'>" + question + "</h1>" +
-				  "<div class = 'a'>" + a + "</div>"+
-				  "<div class = 'b'>" + b + "</div>"+
-				  "<div class = 'c'>" + c + "</div>"+
-				  "<div class = 'd'>" + d + "</div>"+
-				  "<h1 class = 'clock'>" + game.time + "</h1>" 
-
-		$('#display').html(html);		  
-	},			
-
-	//this function displays the correct answer for each question
-	answer1: function(){
-			//if the user guesses the correct answer display the word "correct" and a picture of the composer.
-				if(correct===1){
-					var answer=
-					 "<h1>" + "Correct, Igor Stravinsky wrote the Rite of Spring" + "</h1>" + "<br/>"+
-					 "<img src='assets/images/stravinsky.jpg'/>"
-
-				$('#display').html(answer);
-				}
-			//if the user guesses the incorrect answer, display the word incorrect and a picture of the correct composer. 
-				if(incorrect===1 || unanswered===1){
-					var answer=
-					 "<h1 class='wrong'>" + "Wrong, Igor Stravinsky wrote the Rite of Spring" + "</h1>" + "<br/>"+
-					 "<img src='assets/images/stravinsky.jpg'/>"
-
-				$('#display').html(answer);	 
-				}
-
-	}, 
+    },
+    //this function subtracts game.time by one every time it is called and displays the current game.time in the UI.
+    //it also asks the first multiple choice question and links the question, asnwers and timer to the user Interface.
+    Q1: function() {
+        //this decrements game.time by 1 every second
+        game.time--;
 
 
-	//this function displays the user's results when the game is over and gives them the option to play again.
-	result: function(){ 
+        //console.log should print only when clicked, but it is printing every time the function is called by start.
+        if ($('.a').click()) {
+            console.log("this is working");
+        }
 
-		var end= 
-				  "<h1>" + "Correct:" + correct + "</h1>" +
-				  "<h1>" + "ncsorrect:" + incorrect + "</h1>" +
-				  "<h1>" + "unanswered:" + unanswered + "</h1>" +
-				  "<span id='resetGame'>" + "Play Again"  + "</span>"
+        //this is printing out on the user interface. 
+        question = "Who wrote the Rite of Spring?"
+        a = "Stravinsky";
+        b = "Bach";
+        c = "Mozart";
+        d = "Brahms";
+        game.interface();
 
-		$('#display').html(end);		  
-	}		 		
-	
+
+        //if the timer hits 0, the stop function is called, and answer 1 is called every second.
+        if (game.time === 0) {
+            game.stop();
+            unanswered++;
+            counter = setInterval(game.answer1, 1000);
+        }
+
+    },
+
+    stop: function() {
+        clearInterval(counter);
+        clockRunning = false;
+    },
+
+    //this function resets the game.
+    reset: function() {
+        $('#resetGame').click(game.Q1);
+
+    },
+
+
+    //this function displays the correct answer for each question
+    answer1: function() {
+
+    	//this decrements game.aTime by 1 every second
+        game.aTime--;
+        //if the user guesses the correct answer display the word "correct" and a picture of the composer.
+        if (correct++) {
+            var answer =
+                "<h1>" + "Correct, Igor Stravinsky wrote the Rite of Spring" + "</h1>" + "<br/>" +
+                "<img src='assets/images/stravinsky.jpg'/>"
+
+            $('#display').html(answer);
+        }
+        //if the user guesses the incorrect answer, display the word incorrect and a picture of the correct composer. 
+        if (incorrect ++ || unanswered ++) {
+            var answer =
+                "<h1 class='wrong'>" + "Wrong, Igor Stravinsky wrote the Rite of Spring" + "</h1>" + "<br/>" +
+                "<img src='assets/images/stravinsky.jpg'/>"
+
+            $('#display').html(answer);
+        }
+
+        if (game.aTime === 0) {
+            game.stop();
+            game.timeReset();
+            counter = setInterval(game.Q2, 1000);
+        }
+
+    },
+
+    Q2: function() {
+        //this decrements game.time by 1 every second
+        game.time--;
+
+
+        //console.log should print only when clicked, but it is printing every time the function is called by start.
+        if ($('.a').click()) {
+            console.log("this is working");
+        }
+
+        //this is printing out on the user interface. 
+        question = "Who wrote the Magic Flute?"
+        a = "Haydn";
+        b = "Bach";
+        c = "Mozart";
+        d = "Ligeti";
+        game.interface();
+
+
+        //if the timer hits 0, the stop function is called, and answer 1 is called.
+        if (game.time === 0) {
+            game.stop();
+            unanswered++;
+            counter = setInterval(game.answer2, 1000);
+        }
+
+    },
+
+
+    answer2: function() {
+
+        game.aTime--;
+        //if the user guesses the correct answer display the word "correct" and a picture of the composer.
+        if (correct ++) {
+            var answer =
+                "<h1>" + "Correct, Mozart wrote the Magic Flute" + "</h1>" + "<br/>" +
+                "<img src='assets/images/Mozart.jpg'/>"
+
+            $('#display').html(answer);
+        }
+        //if the user guesses the incorrect answer, display the word incorrect and a picture of the correct composer. 
+        if (incorrect ++|| unanswered ++) {
+            var answer =
+                "<h1 class='wrong'>" + "Wrong, Mozart wrote the Magic Flute" + "</h1>" + "<br/>" +
+                "<img src='assets/images/Mozart.jpg'/>"
+
+            $('#display').html(answer);
+        }
+
+        if (game.aTime = 0) {
+            game.stop();
+            counter = setInterval(game.Q2, 1000);
+        }
+
+    },
+
+
+
+
+    stop: function() {
+        clearInterval(counter);
+        clockRunning = false;
+    },
+
+    timeReset: function() {
+        game.time=20;
+
+    },
+
+    //this function resets the game.
+    reset2: function() {
+        $('#resetGame').click(game.Q1);
+
+    },
+
+
+    //this function is the userInterface that displays when the user is answering questions.
+    interface: function() {
+
+
+        var html =
+            "<h1 class = 'question'>" + question + "</h1>" +
+            "<div class = 'a'>" + a + "</div>" +
+            "<div class = 'b'>" + b + "</div>" +
+            "<div class = 'c'>" + c + "</div>" +
+            "<div class = 'd'>" + d + "</div>" +
+            "<h1 class = 'clock'>" + game.time + "</h1>"
+
+        $('#display').html(html);
+    },
+
+
+    //this function displays the user's results when the game is over and gives them the option to play again.
+    result: function() {
+
+        var end =
+            "<h1>" + "Correct:" + correct + "</h1>" +
+            "<h1>" + "ncsorrect:" + incorrect + "</h1>" +
+            "<h1>" + "unanswered:" + unanswered + "</h1>" +
+            "<span id='resetGame'>" + "Play Again" + "</span>"
+
+        $('#display').html(end);
+    }
+
 
 }
 
